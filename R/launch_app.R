405ebb237fd3d4d4208f617825eb954125069add
vvuu0009 <vvuu0009@student.monash.edu>
2020-10-14 14:37:11 +1100

    Adding launch_app() funciton and documentation

---
+++ R/launch_app.R
@@ -0,0 +1,12 @@
+#' Running Shiny Application
+#'
+#' @description This function runs the Coronavirus Shiny Application
+#'
+#' @return The Shiny Applicaiton 
+#'
+#' @export
+launch_app <- function(){
+  runApp("inst/app/CoronaShiny.R")
+}
+
+runApp("inst/app/CoronaShiny.R")
