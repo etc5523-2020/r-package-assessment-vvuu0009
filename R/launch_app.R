ace480bcf4c36a04ecf590288834488dbdf88363
vvuu0009 <vvuu0009@student.monash.edu>
2020-10-18 17:58:50 +1100

    updating documentation for launch_app function

--- R/launch_app.R
+++ R/launch_app.R
@@ -1,10 +1,13 @@
 #' Running Shiny Application
 #'
-#' @description This function runs the Coronavirus Shiny Application
+#' @description This function runs the Coronavirus Shiny Application exploring the state of Coronavirus as at July 31 2020 
 #'
-#' @return The Shiny Application 
+#' @return The Shiny Application exploring the state of Coronavirus as at July 31 2020 
 #'
 #' @import shiny
+#' 
+#' @example 
+#' launch_app()
 #'
 #' @export
 launch_app <- function(){
