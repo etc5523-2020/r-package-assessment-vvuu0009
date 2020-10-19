798f0e64185fd65f18b2685058448f3d89875db7
vvuu0009 <vvuu0009@student.monash.edu>
2020-10-19 11:50:25 +1100

    Updating documentation for selectInput01

--- R/selectInput01.R
+++ R/selectInput01.R
@@ -1,9 +1,20 @@
 #' Running selectInput for corona app
 #'
 #' @description This function changes the selectInput function of the Shiny app based on the desired input 
+#' 
+#' @param id The variable we wish to include in the selectInput
+#' 
+#' @param column The specific column from the data set we wish to extract the values to appear as the options for the selectInput 
+#' 
+#' @examples 
+#' selectInput01("continent", corona_cont$continent)
+#' 
+#' returns 
+#' 
+#' selectInput("continent", "select the continent", choices = corona_cont$continent)
 #'
 #' 
-#'   @export
+#' @export
 selectInput01 <-
   function(id, column){
     
